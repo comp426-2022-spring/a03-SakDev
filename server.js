2a5f03756636e231c05d1924a18a166b5c873d4e
SakDev <saketh.devareddy1@gmail.com>
2022-04-18 23:22:20 -0400

    doin something ig??

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 
+const args = require('minimist')(process.argv.slice(2))
+
 args["port"]
 
 const port = args["port"] || process.env.PORT || 5000
@@ -64,8 +66,8 @@ function countFlips(array) {
 
 
 // Start an app server
-const server = app.listen(HTTP_PORT, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
 
 app.get('/app/flip/', (req, res) => {
